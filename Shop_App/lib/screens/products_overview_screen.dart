0f5782448d4ca33b02ce8f652ab77d94d0a712db
Nirav Chavda <59245875+niravchavda99@users.noreply.github.com>
2021-02-06 10:14:13 +0530

    Shop App

---
+++ Shop_App/lib/screens/products_overview_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../providers/product.dart';
+import '../widgets/products_grid.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: ProductsGrid(),
+    );
+  }
+}
